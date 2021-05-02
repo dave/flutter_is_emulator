66f92a903b223e5005a18bb99cde407c1511d5b9
Yakup BAŞER <yakupbaser@hotmail.com.tr>
2021-05-02 15:55:36 +0300

    null-safety added

--- lib/flutter_is_emulator.dart
+++ lib/flutter_is_emulator.dart
@@ -6,8 +6,8 @@ class FlutterIsEmulator {
   static const MethodChannel _channel =
       const MethodChannel('g123k/flutter_is_emulator');
 
-  static Future<bool> get isDeviceAnEmulatorOrASimulator async {
-    final bool isEmulatorOrSimulator =
+  static Future<bool?> get isDeviceAnEmulatorOrASimulator async {
+    final bool? isEmulatorOrSimulator =
         await _channel.invokeMethod('isEmulatorOrSimulator');
     return isEmulatorOrSimulator;
   }
